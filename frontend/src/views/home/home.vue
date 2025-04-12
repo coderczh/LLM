d949ce3a89fea2b1e540f758cc6c2027af9e5d18
天天向上 <imchenzh@foxmail.com>
2025-04-12 10:04:52 +0800

    first commit

---
+++ frontend/src/views/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="250px">
+        <left-aside />
+      </el-aside>
+      <el-main>Main</el-main>
+      <el-aside width="200px">Aside</el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import leftAside from '@/components/left-aside/left-aside.vue'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/vite.config.ts
@@ -0,0 +1,52 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      imports: ['vue'],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/style/common.scss" as *;`,
+        api: 'modern-compiler',
+        silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
+})
