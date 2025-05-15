d0d02f295778949f57396306f4ce0f64b214739f
天天向上 <imchenzh@foxmail.com>
2025-05-15 17:10:05 +0800

    20250515_01提交

--- frontend/src/components/tab/login/login.vue
+++ frontend/src/components/tab/login/login.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="dialog">
-    <el-dialog v-model="showDialog" :show-close="false" @close="closeDialog"> </el-dialog>
+    <el-dialog v-model="showDialog" :show-close="false" @close="closeDialog" class="dialog">
+      <div class="dialog-content">
+        <div class="content-login">11</div>
+        <div class="content-img">222</div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -19,7 +24,27 @@ const closeDialog = () => {
 .dialog :deep(.el-dialog) {
   width: 40%;
   height: 300px;
-  margin: 25vh auto 50px;
+  margin: 25vh auto;
+  padding: 10px 10px;
   background: $theme-color-to-right;
+  .dialog-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    .content-login {
+      width: 50%;
+      height: 100%;
+      background-color: red;
+    }
+    .content-img {
+      flex: 1;
+    }
+  }
+}
+
+:deep(.el-dialog__header) {
+  padding-bottom: 0;
 }
 </style>
